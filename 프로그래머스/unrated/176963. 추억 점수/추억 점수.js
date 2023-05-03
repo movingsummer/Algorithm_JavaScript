ee5b66de88d56d638d90414ae17023aeb6468be6
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-04 03:38:03 +0900

    [unrated] Title: 추억 점수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,5 +1,5 @@
 function solution(name, yearning, photo) {
     const score = {};
-    for (let i = 0; i < name.length; i++) score[name[i]] = yearning[i];
+    name.forEach((v, i) => score[v] = yearning[i]);
     return photo.map(v => v.reduce((sum, cur) => sum + (score[cur] ?? 0), 0));
 }
