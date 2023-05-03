f27780e5f2995f3119635fcc4bbdf016600e9814
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-04 02:34:10 +0900

    [unrated] Title: 달리기 경주, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,22 +1,22 @@
 function solution(players, callings) {
-    const swap = (a, b, arr) => {
-        const tmp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = tmp;
-    }
-    
-    const playerRanking = new Map();
-    players.forEach((player, i) => {
-        playerRanking.set(player, i)
-    })
-    
-    callings.forEach(name => {
-        const idx = playerRanking.get(name);
-        const followedName = players[idx - 1]
-        swap(idx, idx - 1, players)
-        playerRanking.set(name, idx - 1)
-        playerRanking.set(followedName, idx)
-    })
-    
-    return players;
+  const swap = (a, b, arr) => {
+    const tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+  };
+
+  const playerRanking = {};
+  players.forEach((player, i) => {
+    playerRanking[player] = i;
+  });
+
+  callings.forEach((name) => {
+    const idx = playerRanking[name];
+    const followedName = players[idx - 1];
+    swap(idx, idx - 1, players);
+    playerRanking[name] = idx - 1;
+    playerRanking[followedName] = idx;
+  });
+
+  return players;
 }
