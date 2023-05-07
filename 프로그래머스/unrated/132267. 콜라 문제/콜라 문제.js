a1346b46613d97075c2d8c39bd3d12ca360a52b0
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-08 04:11:59 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,5 +1,1 @@
-function solution(a, b, n) {
-    if (n < a) return 0;
-    if (n === a) return b;
-    return solution(a, b, n % a + ~~(n / a) * b) + ~~(n / a) * b
-}
+solution = (a, b, n) => ~~((n - b) / (a - b)) * b
