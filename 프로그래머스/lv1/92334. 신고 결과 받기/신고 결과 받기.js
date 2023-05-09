2052ce2a55aceffb1dc91300df0df0861e00aa63
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-09 14:00:47 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,18 +1,13 @@
 function solution(id_list, report, k) {
-    report = Array.from(new Set(report))
-  const reported = new Map();
-  const obj = {};
-  id_list.forEach((v) => {
-    obj[v] = [];
-    reported.set(v, 0);
-  });
-  report.forEach((item) => {
-    const [name1, name2] = item.split(' ');
-    obj[name1].push(name2);
-    reported.set(name2, reported.get(name2) + 1);
-  });
-
-  return id_list.map(
-    (name) => obj[name].filter((v) => reported.get(v) >= k).length
-  );
+    report = Array.from(new Set(report));
+    const reported = new Map();
+    const reporting = new Map();
+    
+    report.forEach(item => {
+        const [name1, name2] = item.split(' ');
+        reported.set(name2, (reported.get(name2) || 0) + 1)
+        reporting.set(name1, (reporting.get(name1) || []).concat([name2]));
+    })
+    
+    return id_list.map(id=>(reporting.get(id) || []).filter(v=>(reported.get(v) || 0) >= k).length)
 }
