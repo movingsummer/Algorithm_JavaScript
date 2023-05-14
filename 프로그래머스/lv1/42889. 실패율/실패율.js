bfea84a0b5f2dbe3d50ba3da62ac36275bd84a9d
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-15 01:51:58 +0900

    [level 1] Title: 실패율, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,28 +1,13 @@
 function solution(N, stages) {
-    stages.sort((a, b) => b - a);
-    const clear = new Array(N + 2).fill(0);
-    let cnt = 0;
-    let pre = stages[0];
-    
-    for (let i = 0; i < stages.length; i++) {
-        const num = stages[i];
-        if (num !== pre) {
-            for (let i = pre; i > num; i--)
-                clear[i] = cnt;
-        }
-        
-        cnt++;
-        pre = num;
+    const people = new Array(N + 2).fill(0);
+    const rate = Array.from(Array(N), (v, i) => [i + 1, 0]);
+    stages.forEach(v=>people[v]++)
+    let ac = 0;
+    for (let i = 0; i < N; i++) {
+        const num = rate[i][0];
+        rate[i][1] = (stages.length - ac) / (stages.length - ac - people[num]) 
+        ac += people[num];
     }
-
-    for (let i = pre; i > 0; i--)
-        clear[i] = cnt;
     
-    
-    console.log(clear)
-    const ans = [];
-    for (let i = 1; i < clear.length - 1; i++)
-        ans.push([i, (clear[i] / clear[i + 1] || 0)]);
-    
-    return ans.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : b[1] - a[1]).map(v=>v[0]);
+    return rate.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : b[1] - a[1]).map(v=>v[0])
 }
