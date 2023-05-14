c6227ebafddab6481217e2f2d8316d1a89631678
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-14 23:02:05 +0900

    [level 1] Title: 모의고사, Time: 2.96 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,11 +1,11 @@
 function solution(answers) {
     const len = answers.length;
-    const g = ['12345'.repeat(Math.ceil(len / 5)),'21232425'.repeat(Math.ceil(len / 8)),'3311224455'.repeat(Math.ceil(len / 10))];
+    const g = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]];
     const s = [0, 0, 0];
     
     for (let i = 0; i < len; i++) {
         for (let j = 0; j < 3; j++)
-        s[j] += (answers[i] === +g[j][i]);
+        s[j] += (answers[i] === +g[j][i % g[j].length]);
     }
     
     const mx = Math.max(...s);
