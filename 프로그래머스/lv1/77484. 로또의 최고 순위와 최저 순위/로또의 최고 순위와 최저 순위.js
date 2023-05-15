14a5effa806b4a4c625e79c16b5dfed0ef4d8525
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-16 01:35:24 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,5 +1,9 @@
 function solution(lottos, win_nums) {
-    const sameNum = lottos.filter(v=>win_nums.includes(v)).length;
-    const z = lottos.filter(v=>v===0).length;
+    const win_nums_map = new Map(win_nums.map(v=>[v, true]));
+    let z = 0;
+    const sameNum = lottos.filter(v=>{
+        if (v === 0) z++;
+        return win_nums_map.get(v) || false
+    }).length;
     return [7 - ((sameNum + z) || 1), 7 - (sameNum || 1)]
 }
