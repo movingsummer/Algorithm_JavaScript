edc2a8c85bf1e9938aa273050da7b9ac9eacaca5
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-15 23:02:30 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,4 +1,3 @@
 function solution(numbers) {
-    const nums = [1,2,3,4,5,6,7,8,9];
-    return nums.filter(v=>!numbers.includes(v)).reduce((s, t) => s + t);
+    return 45 - numbers.reduce((s, v) => s + v);
 }
