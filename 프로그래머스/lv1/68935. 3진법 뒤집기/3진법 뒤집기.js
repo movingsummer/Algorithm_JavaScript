dc127fc44329d1292daf7af305a82650bae92e73
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-15 11:13:03 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,9 +1,3 @@
 function solution(n) {
-    const t = [];
-    while (n > 0) {
-        t.push(n % 3);
-        n = ~~(n / 3);
-    }
-    
-    return t.reverse().reduce((sum, v, i) => sum + v * (3 ** i), 0)
+    return [...n.toString(3)].reduce((sum, v, i) => sum + v * (3 ** i), 0)
 }
