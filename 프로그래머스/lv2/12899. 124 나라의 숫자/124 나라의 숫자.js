ec211e9f0f77416fe1e87fd401b8061e520a3fd1
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-16 15:30:41 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.js
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -1,14 +1,3 @@
 function solution(n) {
-    const map = {0:"1",1:"2",2:"4"};
-    const ret = [];
-    
-    while (n > 0) {
-        n -= 1;
-        ret.push(map[n % 3]);
-        n = ~~(n / 3);
-    }
-    
-    return ret.reverse().join('')
+    return n ? solution(~~((n - 1) / 3)) + [1, 2, 4][(n - 1) % 3] : ''
 }
-
-
