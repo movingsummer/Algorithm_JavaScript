037e167f98429c09c595788c8c5bfed9695059dc
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-28 16:09:57 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 144 ms, Memory: 10120 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.js
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.js
@@ -27,62 +27,72 @@ class Queue {
   }
 }
 
-const [dx, dy] = [
-  [1, 0, -1, 0],
-  [0, 1, 0, -1],
-];
-const OOB = (x, y) => x < 0 || x >= n || y < 0 || y >= m;
-const go = (x, y, dir, ax, ay) => {
-  let flag = 0;
-
-  while (
-    !OOB(x + dx[dir], y + dy[dir]) &&
-    board[x + dx[dir]][y + dy[dir]] !== '#'
-  ) {
-    const nx = x + dx[dir];
-    const ny = y + dy[dir];
-
-    if (board[nx][ny] === 'O') return [-1, -1];
-    if (nx === ax && ny === ay) flag = 1;
-    x = nx;
-    y = ny;
-  }
-
-  return [x - flag * dx[dir], y - flag * dy[dir]];
-};
-const [nm, ...board] = (require('fs').readFileSync('/dev/stdin') + '')
+let ans = -1;
+const [nm, ...b] = (require('fs').readFileSync('/dev/stdin') + '')
   .trim()
   .split(/\r?\n/);
 const [n, m] = nm.split(' ').map((v) => +v);
+const board = b.map((v) => v.split(''));
 let red, blue, dest;
-let ans = -1;
 
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < m; j++) {
-    if (board[i][j] === 'R') red = [i, j];
-    if (board[i][j] === 'B') blue = [i, j];
+    if (board[i][j] === 'R') {
+      red = [i, j];
+      board[i][j] = '.';
+    }
+    if (board[i][j] === 'B') {
+      blue = [i, j];
+      board[i][j] = '.';
+    }
     if (board[i][j] === 'O') dest = [i, j];
   }
 }
 
+const [dx, dy] = [
+  [1, 0, -1, 0],
+  [0, 1, 0, -1],
+];
 const q = new Queue();
+const vis = Array.from(Array(n), () =>
+  Array.from(Array(m), () => Array.from(Array(n), () => new Array(m).fill(0)))
+);
 q.push([red[0], red[1], blue[0], blue[1], 0]);
+vis[red[0]][red[1]][blue[0]][blue[1]] = 1;
 
 a: while (q.length) {
-  const cur = q.pop();
-  if (cur[4] >= 10) break;
+  const [rx, ry, bx, by, num] = q.pop();
+  if (num >= 10) break;
   for (let dir = 0; dir < 4; dir++) {
-    const [rx, ry] = go(cur[0], cur[1], dir, cur[2], cur[3]);
-    const [bx, by] = go(cur[2], cur[3], dir, cur[0], cur[1]);
-    if (rx === -1 && ry === -1) {
-      if (bx === -1 && by === -1) continue;
-      else {
-        ans = cur[4] + 1;
-        break a;
-      }
+    let [n_rx, n_ry, n_bx, n_by] = [rx, ry, bx, by];
+
+    while (board[n_bx + dx[dir]][n_by + dy[dir]] === '.') {
+      n_bx += dx[dir];
+      n_by += dy[dir];
     }
-    if (bx === -1 && by === -1) continue;
-    q.push([rx, ry, bx, by, cur[4] + 1]);
+
+    if (n_bx + dx[dir] === dest[0] && n_by + dy[dir] === dest[1]) continue;
+
+    while (board[n_rx + dx[dir]][n_ry + dy[dir]] === '.') {
+      n_rx += dx[dir];
+      n_ry += dy[dir];
+    }
+
+    if (n_rx + dx[dir] === dest[0] && n_ry + dy[dir] === dest[1]) {
+      ans = num + 1;
+      break a;
+    }
+
+    if (n_rx === n_bx && n_ry === n_by) {
+      if (dir === 0) rx > bx ? n_bx-- : n_rx--;
+      else if (dir === 1) ry > by ? n_by-- : n_ry--;
+      else if (dir === 2) rx < bx ? n_bx++ : n_rx++;
+      else ry < by ? n_by++ : n_ry++;
+    }
+
+    if (vis[n_rx][n_ry][n_bx][n_by]) continue;
+    q.push([n_rx, n_ry, n_bx, n_by, num + 1]);
+    vis[n_rx][n_ry][n_bx][n_by] = 1;
   }
 }
 
