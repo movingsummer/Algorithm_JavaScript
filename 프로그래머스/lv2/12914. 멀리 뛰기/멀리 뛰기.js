a938b762d9ca4e02490434dd3aa0b77f665c13bc
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-05-30 22:31:00 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    const d = new Array(n + 1).fill(0);
+    const mod = 1234567;
+    d[1] = 1;
+    d[2] = 2;
+    for (let i = 3; i <= n; i++) d[i] = d[i - 1] % mod + d [i-2] % mod
+    return d[n] % mod
+}
