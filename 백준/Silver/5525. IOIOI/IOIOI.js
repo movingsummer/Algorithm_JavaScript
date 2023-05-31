6ed44a3f23d0b78e0968f3c6fd36a7d0cc706835
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-06-01 00:58:31 +0900

    [Silver I] Title: IOIOI, Time: 192 ms, Memory: 11872 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -2,37 +2,20 @@ let [n, m, str] = (require('fs').readFileSync('/dev/stdin') + '')
   .trim()
   .split(/\r?\n/);
 [n, m] = [+n, +m];
-let ans = 0;
-let d = [];
-let cnt = 0;
-let mode = 0; // 0이면 보통, 1이면 탐색중
 
-for (let i = 0; i < str.length; i++) {
-  const c = str[i];
+let [cnt, ans] = [0, 0];
 
-  if (mode === 1) {
-    if (c !== str[i - 1]) {
-      cnt++;
-      if (cnt >= 3) {
-        if (cnt % 2) d.push(~~((cnt - 3) / 2) + 1);
-      }
-    } else {
-      if (c === 'O') {
-        cnt = 0;
-        mode = 0;
-      } else if (c === 'I') {
-        cnt = 1;
-      }
-      ans += d.filter((v) => v >= n).length;
-      d = [];
+for (let i = 1; i < m - 1; i++) {
+  if (str[i - 1] === 'I' && str[i] === 'O' && str[i + 1] === 'I') {
+    cnt++;
+    i++;
+    if (n === cnt) {
+      ans++;
+      cnt--;
     }
-  } else if (mode === 0) {
-    if (c === 'I') {
-      mode = 1;
-      cnt = 1;
-    }
+  } else {
+    cnt = 0;
   }
 }
 
-ans += d.filter((v) => v >= n).length;
 console.log(ans);
