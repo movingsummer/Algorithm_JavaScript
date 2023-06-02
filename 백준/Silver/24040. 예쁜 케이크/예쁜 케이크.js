69923d8351f43b440ad993373256df7e135e04c9
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-06-02 19:37:22 +0900

    [Silver II] Title: 예쁜 케이크, Time: 120 ms, Memory: 9628 KB -BaekjoonHub

---
+++ 백준/Silver/24040. 예쁜 케이크/예쁜 케이크.js
@@ -0,0 +1,17 @@
+const arr = ['NIE', 'TAK'];
+const [t, ...nums] = (require('fs').readFileSync('/dev/stdin') + '')
+  .trim()
+  .split(/\r?\n/);
+console.log(
+  nums.reduce((ans, v) => {
+    v = BigInt(v);
+    let flag = 0;
+    if (v % 3n === 2n) flag = 1;
+    else if (v % 3n === 0n) {
+      if (v % 9n === 0n) flag = 1;
+    } else {
+      if (v % 2n === 0) flag = 1;
+    }
+    return ans + arr[flag] + '\n';
+  }, '')
+);
