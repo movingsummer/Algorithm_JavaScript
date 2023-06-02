2c5caf8eb68788827a69eb84bb6ced4be37e46c7
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-06-03 00:58:43 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,8 +1,5 @@
 function solution(arr) {
-    arr.sort((a, b) => b - a);
-    const mx = arr[0];
-    for (let i = 1; ; i++) {
-        const v = mx * i;
-        if (arr.every(e=>v%e===0)) return v
-    }
+    return arr.reduce((ans, v) => ans * v / gcd(ans, v), 1)
 }
+
+const gcd = (a, b) => a % b ? gcd(b, a % b) : b;
