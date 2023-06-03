c6896a716a0656edb8d622fa1f2cb3e4739226b7
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-06-03 23:23:50 +0900

    [Silver III] Title: 어린 왕자, Time: 196 ms, Memory: 12364 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.js
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.js
@@ -16,10 +16,7 @@ while (tc--) {
     const [cx, cy, r] = item.split(' ').map((v) => +v);
     const d1 = Math.sqrt((cx - x2) ** 2 + (cy - y2) ** 2); // 출발점, 해당 원 거리
     const d2 = Math.sqrt((x1 - cx) ** 2 + (y1 - cy) ** 2); // 도착점. 해당 원 거리
-    if (!(d1 < r && d2 < r)) {
-      if (d1 < r) cnt++;
-      if (d2 < r) cnt++;
-    }
+    cnt += (d1 < r) ^ (d2 < r);
   });
 
   ans.push(cnt);
