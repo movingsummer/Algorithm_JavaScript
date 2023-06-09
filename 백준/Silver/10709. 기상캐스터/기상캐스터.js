ca4dd58194213ad0cf703d29038aaa41118db0cc
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-06-09 22:49:05 +0900

    [Silver V] Title: 기상캐스터, Time: 120 ms, Memory: 9672 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.js
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.js
@@ -2,18 +2,12 @@ const [hw, ...JCity] = (require('fs').readFileSync('/dev/stdin') + '')
   .trim()
   .split(/\r?\n/);
 const [h, w] = hw.split(' ').map((v) => +v);
-const JCityMap = JCity.map((v) => v.split(''));
+const JCityMap = JCity.map((v) => v.split('').map((v) => (v === '.' ? -1 : 0)));
 
 for (let i = 0; i < h; i++) {
-  let cnt = -1;
-  for (let j = 0; j < w; j++) {
-    if (JCityMap[i][j] === '.') {
-      if (cnt === -1) JCityMap[i][j] = -1;
-      else JCityMap[i][j] = cnt++;
-    } else {
-      cnt = 0;
-      JCityMap[i][j] = cnt++;
-    }
+  for (let j = 1; j < w; j++) {
+    if (JCityMap[i][j - 1] !== -1 && JCityMap[i][j] !== 0)
+      JCityMap[i][j] = JCityMap[i][j - 1] + 1;
   }
 }
 
