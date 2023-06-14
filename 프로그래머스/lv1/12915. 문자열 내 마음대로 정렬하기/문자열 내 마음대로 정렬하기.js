cba9304d17aaf81b28ec794a5e362a88e810bb01
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-06-14 17:34:52 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,7 +1,3 @@
 function solution(strings, n) {
-    for (let i = 0; i < strings.length; i++) {
-        strings[i] = strings[i][n] + strings[i];
-    }
-    
-    return strings.sort().map(v=>v.replace(v[0], ""))
+    return strings.map(v=>v[n]+v).sort().map(v=>v.replace(v[0], ""))
 }
