37863e774630707e8b41ae4592324edba18ef68e
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-06-19 18:24:31 +0900

    [unrated] Title: 달리기 경주, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,22 +1,30 @@
 function solution(players, callings) {
-  const swap = (a, b, arr) => {
-    const tmp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = tmp;
-  };
-
-  const playerRanking = {};
-  players.forEach((player, i) => {
-    playerRanking[player] = i;
-  });
-
-  callings.forEach((name) => {
-    const idx = playerRanking[name];
-    const followedName = players[idx - 1];
-    swap(idx, idx - 1, players);
-    playerRanking[name] = idx - 1;
-    playerRanking[followedName] = idx;
-  });
-
-  return players;
+    
+    const swap = (num1, num2) => {
+        const [name1, name2] = [rankingNum.get(num1), rankingNum.get(num2)];
+        rankingName.set(name1, num2);
+        rankingName.set(name2, num1);
+        rankingNum.set(num1, name2);
+        rankingNum.set(num2, name1);
+    }
+    
+    const ans = [];
+    const rankingName = new Map();
+    const rankingNum = new Map();
+    
+    players.forEach((playerName, i) => {
+        rankingName.set(playerName, i + 1);
+        rankingNum.set(i + 1, playerName);
+    })
+    
+    callings.forEach(playerName => {
+        const playerNum = rankingName.get(playerName);
+        swap(playerNum - 1, playerNum);
+    })
+    
+    for (let i = 0; i < players.length; i++) {
+        ans.push(rankingNum.get(i + 1));
+    }
+    
+    return ans;
 }
