0f339aef45dac407d2847364e1ef47d1f3da598d
LittlePieceofYou <111179843+LittlePieceofYou@users.noreply.github.com>
2023-06-23 16:57:38 +0900

    [Silver III] Title: 눈 치우기, Time: 124 ms, Memory: 9396 KB -BaekjoonHub

--- 백준/Silver/26215. 눈 치우기/눈 치우기.js
+++ 백준/Silver/26215. 눈 치우기/눈 치우기.js
@@ -1,71 +1,74 @@
 class Heap {
-  #heap;
+  #comp;
+  #size;
+  #pqArr;
+
   constructor(comp = (a, b) => a - b) {
-    this.#heap = [0];
-    this.comp = comp;
+    this.#size = 0;
+    this.#pqArr = [undefined];
+    this.#comp = comp;
   }
 
   push(data) {
-    let idx = this.#heap.length;
-    this.#heap.push(data);
+    this.#pqArr.push(data);
+    this.#size++;
+    let idx = this.#size;
 
-    while (idx != 1) {
-      if (this.comp(this.#heap[~~(idx / 2)], data) > 0) {
-        this.#heap[idx] = this.#heap[~~(idx / 2)];
+    while (idx !== 1) {
+      if (this.#comp(data, this.#pqArr[~~(idx / 2)]) < 0) {
+        this.#pqArr[idx] = this.#pqArr[~~(idx / 2)];
         idx = ~~(idx / 2);
       } else break;
     }
 
-    this.#heap[idx] = data;
-  }
-
-  top() {
-    return this.#heap[1];
+    this.#pqArr[idx] = data;
   }
 
   pop() {
-    if (this.size === 0) return 0;
-    const data = this.#heap[this.size];
-    const retData = this.#heap[1];
-    let idx = 1;
+    const retElem = this.#pqArr[1];
+    const lastElem = this.#pqArr[this.#size--];
+    let pIdx = 1;
+    let cIdx;
 
-    while (idx * 2 + 1 <= this.size) {
-      let childIdx = 0;
-      if (this.comp(this.#heap[idx * 2], this.#heap[idx * 2 + 1]) > 0)
-        childIdx = idx * 2 + 1;
-      else childIdx = idx * 2;
+    while (true) {
+      cIdx =
+        this.#comp(this.#pqArr[pIdx * 2], this.#pqArr[pIdx * 2 + 1]) < 0
+          ? pIdx * 2
+          : pIdx * 2 + 1;
 
-      if (this.comp(data, this.#heap[childIdx]) > 0) {
-        this.#heap[idx] = this.#heap[childIdx];
-        idx = childIdx;
+      if (cIdx > this.#size) break;
+
+      if (this.#comp(lastElem, this.#pqArr[cIdx]) > 0) {
+        this.#pqArr[pIdx] = this.#pqArr[cIdx];
+        pIdx = cIdx;
       } else break;
     }
 
-    this.#heap[idx] = data;
-    this.#heap.pop();
-    return retData;
+    this.#pqArr[pIdx] = lastElem;
+
+    return retElem;
   }
 
-  get size() {
-    return this.#heap.length - 1;
+  top() {
+    return this.#pqArr[1];
+  }
+
+  get _size() {
+    return this.#size;
   }
 }
 
-const [[n], snow] = (require('fs').readFileSync('/dev/stdin') + '')
-  .trim()
-  .split(/\r?\n/)
-  .map((v) => v.split(' ').map((v2) => +v2));
-let ans = 0;
+const [[n], snow] = (require('fs').readFileSync('/dev/stdin') + '').trim().split(/\r?\n/).map(v=>v.split(' ').map(v2=>+v2));
 const pq = new Heap((a, b) => b - a);
-snow.forEach((v) => pq.push(v));
+let time = 0;
+snow.forEach(v=>pq.push(v));
 
-while (pq.size > 1) {
-  const [fir, sec] = [pq.pop(), pq.pop()];
-  ans += sec;
-  pq.push(fir - sec);
+while (pq._size > 1) {
+    const [fir, sec] = [pq.pop(), pq.pop()];
+    time += sec;
+    pq.push(fir - sec);
 }
 
-ans += pq.pop();
-
-if (ans > 1440) console.log(-1);
-else console.log(ans);
+time += pq.pop();
+if (time > 1440) time = -1;
+console.log(time);
